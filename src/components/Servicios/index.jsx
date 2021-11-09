fda07ba47f59e176383e59179b80a539cfd76f8c
astrla <fabian.trigo@alumnos.uv.cl>
2021-11-09 01:07:22 -0300

    se comienza el desarrollo de Servicios

---
+++ src/components/Servicios/index.jsx
@@ -0,0 +1,21 @@
+import { Grid, Container } from "@material-ui/core";
+
+
+
+const Servicios = () => {
+    /* Descripcion del componente:
+       Se listan algunas categorias de manera vertical | | | | 
+       combinando opciones de trasparencia de CSS y Grid de Material UI
+    
+    */
+    return (
+        <div>
+           <h1 className="white">Servicios</h1> 
+            <Grid container spacing={4}>
+
+            </Grid>
+        </div>
+    )
+}
+
+export default Servicios
